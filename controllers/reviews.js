fdffdcbf915f6d963f311e13a06e99f70aab3b74
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-19 17:14:49 +0700

    Post route added

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -7,5 +7,12 @@ router.get('/', (req, res) => {
   res.render('/reviews')
   console.log('Hello from Reviews')
 })
+
+//Routes
+//POST /
+router.post('/', (req, res) => {
+  res.send('your review has been sent')
+  console.log('review submitted')
+})
 // Export
 module.exports = router
