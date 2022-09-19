4c62ef00b1129918769a9049d88062038bfca651
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-19 15:53:30 +0700

    Updated controller

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -3,6 +3,9 @@ const express = require('express')
 const router = express.Router()
 
 // Create here a controller
-
+router.get('/', (req, res) => {
+  res.render('bookings')
+  console.log('Hello from Bookings')
+})
 // Export
 module.exports = router
