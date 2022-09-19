aea304c4d0850415327429a24b5a15b4eed9f2a9
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-19 15:53:42 +0700

    Updated controller

--- controllers/profile.js
+++ controllers/profile.js
@@ -3,6 +3,9 @@ const express = require('express')
 const router = express.Router()
 
 // Create here a controller
-
+router.get('/', (req, res) => {
+  res.render('profile')
+  console.log('Hello from Profile')
+})
 // Export
 module.exports = router
