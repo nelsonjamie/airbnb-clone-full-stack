e25be287f22ee51e5590e19ddf5db06b9e8dd135
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-19 15:53:47 +0700

    Updated controller

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,6 +3,9 @@ const express = require('express')
 const router = express.Router()
 
 // Create here a controller
-
+router.get('/', (req, res) => {
+  res.render('/reviews')
+  console.log('Hello from Reviews')
+})
 // Export
 module.exports = router
