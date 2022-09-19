d394181b3f2fb4309b0b40577d2e47638884ce1e
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-19 15:53:25 +0700

    Updated controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,10 @@ const express = require('express')
 const router = express.Router()
 
 // Create here a controller
+router.get('/', (req, res) => {
+  res.render('/auth')
+  console.log('Hello from Auth')
+})
 
 // Export
 module.exports = router
