be752e56a4bbb54b3b506f3db98768086439d2a4
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-19 15:53:35 +0700

    Updated controller

--- controllers/houses.js
+++ controllers/houses.js
@@ -3,6 +3,9 @@ const express = require('express')
 const router = express.Router()
 
 // Create here a controller
-
+router.get('/', (req, res) => {
+  res.render('/houses')
+  console.log('Hello from Houses')
+})
 // Export
 module.exports = router
