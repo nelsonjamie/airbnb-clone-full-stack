ff466597af963f794a0b089957cbfd6cf691721f
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-19 15:53:38 +0700

    Updated controller

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const router = express.Router()
 
 // Create here a controller
-
+router.get('/', (req, res) => {
+  res.render('index')
+  console.log('Hello from Index')
+})
 // Export
 module.exports = router
