d53e2c9326db9dbfa6ebc4a2d6571f63b6282e8c
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-19 17:14:34 +0700

    Routes added

--- controllers/profile.js
+++ controllers/profile.js
@@ -7,5 +7,18 @@ router.get('/', (req, res) => {
   res.render('profile')
   console.log('Hello from Profile')
 })
+
+//Routes
+// GET /
+router.get('/', (req, res) => {
+  res.send('Profile')
+  console.log('profile displayed')
+})
+// PATCH /
+router.patch('/', (req, res) => {
+  res.send('Profile updated')
+  console.log('profile updated')
+})
+
 // Export
 module.exports = router
