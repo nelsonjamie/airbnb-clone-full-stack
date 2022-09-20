43987cf6a56a8946059de7c5a5742a6d769a1c04
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-20 14:21:24 +0700

    Created users schema

---
+++ models/users.js
@@ -0,0 +1,15 @@
+let usersModel = mongoose.model('users', {
+  avatar: Text,
+  email: {
+    type: Text,
+    require: true
+  },
+  name: {
+    type: Text,
+    require: true
+  },
+  password: {
+    type: Text,
+    require: true
+  }
+})
