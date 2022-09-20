5edea2bd35dda24c954ac2be04c7213b6b95592d
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-20 13:24:47 +0700

    Removed route

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,10 +3,6 @@ const express = require('express')
 const router = express.Router()
 
 // Create here a controller
-router.get('/', (req, res) => {
-  res.render('/auth')
-  console.log('Hello from Auth')
-})
 
 //Routes
 
