f148ae97b7ac99f1a5c1b45a29e840af6e891009
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-22 18:36:34 +0700

    Deleted helpers

--- helpers/hbs-helpers.js
+++
@@ -1,7 +0,0 @@
-const hbs = require('hbs')
-
-hbs.registerHelper('ifEq', (arg1, arg2, options) => {
-  return String(arg1) == String(arg2) ? options.fn(this) : options.inverse(this)
-})
-
-module.exports = hbs
