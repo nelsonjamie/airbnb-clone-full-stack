d289890adeb70e080930ab775cc9a1430ca52a2f
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-22 18:12:22 +0700

    Created helper ifEq

---
+++ helpers/hbs-helpers.js
@@ -0,0 +1,7 @@
+const hbs = require('hbs')
+
+hbs.registerHelper('ifEq', (arg1, arg2, options) => {
+  return String(arg1) == String(arg2) ? options.fn(this) : options.inverse(this)
+})
+
+module.exports = hbs
