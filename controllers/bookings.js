f33265e47f0ccea0aaefe303e6d1d9e4384d2164
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-23 15:51:02 +0700

    Started bookings post controller

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -3,10 +3,8 @@ const express = require('express')
 const router = express.Router()
 
 // Create here a controller
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   if (req.isAuthenticated()) {
-    res.send('your booking has been sent')
-    console.log('req.body')
   } else {
     res.redirect('/login')
   }
