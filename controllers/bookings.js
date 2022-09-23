c3fe27182d3d41714f3d738b6d31ea60c24339e8
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-23 16:44:24 +0700

    Updated post controller for bookings

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -2,9 +2,16 @@
 const express = require('express')
 const router = express.Router()
 
+const Bookings = require('../models/bookings')
+
 // Create here a controller
 router.post('/', async (req, res) => {
   if (req.isAuthenticated()) {
+    console.log('this is the thing', req.body)
+    req.body.user = req.user._id
+
+    let booking = await Bookings.create(req.body)
+    res.redirect(`/houses/${req.body.house}`)
   } else {
     res.redirect('/login')
   }
